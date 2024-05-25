81c58e0da40eb72c4b583bb13424d536ba13fa15
ayman nawwar <darkmagiccode.com>
2024-05-25 23:08:36 +0300

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => {
   // destructuring items from config object
   const { favicon } = config.site;
+  const baseUrl = window.location.origin;
+  const currentPath = window.location.pathname;
   return (
     <Html lang='en'>
       <Head>
@@ -16,10 +18,12 @@ const Document = () => {
         <meta name='theme-name' content='AlHazam-light-nextjs' />
         <meta name='msapplication-TileColor' content='#000000' />
         <meta name='theme-color' media='(prefers-color-scheme: light)' content='#fff' />
-        {/* <meta
-          name='og:image'
-          content='https://scontent-hbe1-2.xx.fbcdn.net/v/t39.30808-6/307486267_478264784323046_501080229019256398_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGyY-5dCdHvw9S34xTiaezM0RkwMFACcyjRGTAwUAJzKEeIPJb6YZbstNbv152yZQdkgWjRRTJC83lGW902b77i&_nc_ohc=GZVYPhrYRuoQ7kNvgGJeI0I&_nc_ht=scontent-hbe1-2.xx&oh=00_AYA64S0saHLVVqMNHhIyrUXDvhGEd-x0h-KB-9C4Q2guEA&oe=6656B83A'
-        /> */}
+        {currentPath == '/' && (
+          <meta
+            name='og:image'
+            content='https://scontent-hbe1-2.xx.fbcdn.net/v/t39.30808-6/307486267_478264784323046_501080229019256398_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGyY-5dCdHvw9S34xTiaezM0RkwMFACcyjRGTAwUAJzKEeIPJb6YZbstNbv152yZQdkgWjRRTJC83lGW902b77i&_nc_ohc=GZVYPhrYRuoQ7kNvgGJeI0I&_nc_ht=scontent-hbe1-2.xx&oh=00_AYA64S0saHLVVqMNHhIyrUXDvhGEd-x0h-KB-9C4Q2guEA&oe=6656B83A'
+          />
+        )}
 
         <meta name='theme-color' media='(prefers-color-scheme: dark)' content='#000' />
       </Head>
