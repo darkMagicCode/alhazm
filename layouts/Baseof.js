1b86007bec06c9313548075c7bae5f760d788cf9
ayman nawwar <darkmagiccode.com>
2024-06-06 19:39:01 +0300

    fix

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -113,11 +113,11 @@ const Base = ({ title, meta_title, description, image, noindex, canonical, child
         {/* {pathname !== '/' ? (
           <meta name='og:image' content={`${base_url}_next/image?url=%2Fimages%2F${folder}%2F${name}&w=1200&q=75`} />
         ) : ( */}
-        <meta name='og:image' content={logo} />
+        {/* <meta name='og:image' content={logo} /> */}
         {/* )} */}
         {/* <meta name='og:image' content={`${base_url}_next/image?url=%2Fimages%2Fcar1%2F3.jpg&w=1200&q=75`} /> */}
-        <meta property='og:image:width' content='1200' />
-        <meta property='og:image:height' content='630' />
+        {/* <meta property='og:image:width' content='1200' /> */}
+        {/* <meta property='og:image:height' content='630' /> */}
         {/* twitter-image */}
         <meta name='twitter:image' content={`${base_url}${image ? image : meta_image}`} />
         <meta name='twitter:card' content='summary_large_image' />
